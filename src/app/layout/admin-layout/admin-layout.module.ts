e778078e792103fcb90d952c42a8cd123da7525b
sbongi-seni <109135904+sbongi-seni@users.noreply.github.com>
2024-01-15 11:03:14 +0200

    Update SWAS-65

--- src/app/layout/admin-layout/admin-layout.module.ts
+++ src/app/layout/admin-layout/admin-layout.module.ts
@@ -1,12 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminLayoutRoutingModule } from './admin-layout-routing.module';
-// import { DashboardComponent } from 'src/app/pages/dashboard/dashboard.component';
+import { DashboardComponent } from 'src/app/pages/dashboard/dashboard.component';
 
 
 @NgModule({
   imports: [
   ],
-  // declarations: [DashboardComponent],
+   declarations: [DashboardComponent],
 })
 export class AdminLayoutModule {}
