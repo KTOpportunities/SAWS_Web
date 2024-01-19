e94fbf21e704a65221ed234595db13409811ba43
TahulelaN <145351126+TahulelaN@users.noreply.github.com>
2024-01-19 14:26:42 +0200

    Forgot Password New Changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { environment } from "../../../environment";
-import { from } from "rxjs";
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { environment } from '../../../environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { catchError, from } from 'rxjs';
 
 interface loginform {
   Username: string;
