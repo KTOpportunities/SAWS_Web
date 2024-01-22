0db5748a1a1f634301ee23e50d424d65ce45c69d
mancobakto <150122296+mancobakto@users.noreply.github.com>
2024-01-22 17:42:37 +0200

    SAWS-76 - implement reset password

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { environment } from '../../../environment';
 import { ReactiveFormsModule } from '@angular/forms';
 import { catchError, from } from 'rxjs';
+import { ResetConfirmPassword, ResetPassword } from 'src/app/models/resetPassword';
+
 
 interface loginform {
   Username: string;
@@ -33,11 +35,20 @@ export class AuthService {
       form
     );
   }
-  requestPasswordReset(form: any){
-    this.body.email = form.username;
-    debugger
-    return this.http.post<any>(environment.serverAPI + `Authenticate/RequestPasswordReset?email=${this.body.email}`, this.body.email);
+
+  requestPasswordReset(form: ResetPassword){
+    return this.http.post<ResetPassword>(environment.serverAPI + `Authenticate/RequestPasswordReset?email=${form.email}`, {});
   }
+
+  resetPassword(form: ResetConfirmPassword) {
+   return this.http.post<ResetConfirmPassword>(environment.serverAPI + 'Authenticate/ResetPassword', form);
+  }
+
+  // requestPasswordReset(form: any){
+  //   this.body.email = form.username;
+  //   debugger
+  //   return this.http.post<any>(environment.serverAPI + `Authenticate/RequestPasswordReset?email=${this.body.email}`, this.body.email);
+  // }
   
 
   RequestPasswordReset(form: any) {
