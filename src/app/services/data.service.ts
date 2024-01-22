ac7f422235557bc7d70441db17db27dad85aa6fa
mancobakto <150122296+mancobakto@users.noreply.github.com>
2024-01-22 16:08:26 +0200

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,22 +8,9 @@ interface User {}
 })
 
 export class DataService {
-  // userInformation: any = '';
   private sharedObject: any;
-  // private UserInformation = new BehaviorSubject<any>(this.userInformation);
 
   constructor() {}
-  // saveCurrentUser(user: UserLoggedIn): string {
-  //   sessionStorage.setItem('User', JSON.stringify(user));
-  //   sessionStorage.setItem('token', user.DetailDescription.token);
-  //   return 'User Saved';
-  // }
-  // getCurrentUser() {
-  //   return sessionStorage.getItem('User');
-  // }
-  // getToken(): string | null {
-  //   return window.sessionStorage.getItem('token');
-  // }
 
   setSharedObject(obj: any): void {
     this.sharedObject = obj;
