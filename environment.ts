08c154cca1472ba68a6f62b1feb91a5d65389990
mancobakto <150122296+mancobakto@users.noreply.github.com>
2024-08-14 16:13:07 +0200

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    serverAPI: 'http://160.119.253.130/aviappapi/api/',
+    serverAPI: 'http://dev-testd.saws.co.za:81/api/',
+    // serverAPI: 'http://160.119.253.130/aviappapi/api/',
 }
