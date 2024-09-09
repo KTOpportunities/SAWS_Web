2fd18967f9c76e658347394d6434d22b444996fe
mancobakto <150122296+mancobakto@users.noreply.github.com>
2024-09-09 21:58:58 +0200

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     serverAPI: 'http://aviapp.weathersa.co.za:81/api/',
     // serverAPI: 'http://160.119.253.130/aviappapi/api/',
+    //serverAPI: 'https://localhost:7031/api/',
 }
